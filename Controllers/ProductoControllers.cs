d57b7bc4c4a9e83dddd3bc1bf5b358a177dd0de0
Agustina Gallardo <agusgallardo275@gmail.com>
2022-10-14 10:26:48 -0300

    GET y Post

---
+++ Controllers/ProductoControllers.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProductosWebAPI.Model;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ProductosWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductoControllers : ControllerBase
+    {
+        private static ProductoControllers Instancia;
+
+        private static readonly List<Producto> lstProductos = new List<Producto>(); 
+
+
+        public static ProductoControllers ObtenerInstancia()
+        {
+            if (Instancia == null)
+            {
+                Instancia = new ProductoControllers();
+            }
+            return Instancia;
+        }
+
+
+        // GET: api/Producto
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(lstProductos);    
+        }
+
+        // GET api/Producto/
+        [HttpGet("{nombre}")]
+        public IActionResult Get(string nombre)
+        {
+            foreach(Producto p in lstProductos)
+            {
+                if(p.NombreProducto.Equals(nombre))
+                {
+                    return Ok(p);
+                }
+            }
+            return NotFound("No existe ese producto");
+        }
+
+        // POST api/Producto
+        [HttpPost]
+        public IActionResult Post([FromBody] Producto value)
+        {
+            if (value == null || string.IsNullOrEmpty(value.NombreProducto))
+                return BadRequest("Error datos incorrectos");
+            lstProductos.Add(value);
+            return Ok("Se agrego el producto con exito");
+        }
+
+        // PUT api/<Producto>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<Producto>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Model/Producto.cs
@@ -0,0 +1,11 @@
+﻿namespace ProductosWebAPI.Model
+{
+    public class Producto
+    {
+        public int CodigoProducto { get; set; }
+        public string NombreProducto { get; set; }
+        public float Precio { get; set; }   
+
+
+    }
+}
